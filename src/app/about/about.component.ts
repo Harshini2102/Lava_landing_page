80a74a9b79152fef96473013de1c1a5c75030940
harshini.shah.jira <harshini.shah@azilen.com>
2021-01-06 17:11:53 +0530

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {HomeComponent} from '../home/home.component';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
